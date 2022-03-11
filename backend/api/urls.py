5d7977bf20e041f015da1723bbcc8ad6f231b6c9
hamed hajioupour <44294983+trader86@users.noreply.github.com>
2022-03-11 22:50:59 +0330

    permissions

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,7 +5,7 @@ app_name = 'api'
 
 urlpatterns = [
     path('', ArticleList.as_view(), name='article-list'),
-    path('<slug:slug>', ArticleDetail.as_view(), name='article-detail'),
+    path('<int:pk>', ArticleDetail.as_view(), name='article-detail'),
     path('users/', UserList.as_view(), name='user-list'),
     path('users/<int:pk>', UserDetail.as_view(), name='user-detail'),
     
